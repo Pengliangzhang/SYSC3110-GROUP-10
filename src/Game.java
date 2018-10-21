85fa3b736e14dce76c41fe78eac5d9ee638138e2
pengliangzhang <Pengliangzhang@cmail.carleton.ca>
2018-10-21 15:51:19 -0400

    test

--- src/Game.java
+++ src/Game.java
@@ -1,24 +1,30 @@
 
 public class Game
 {
-<<<<<<< HEAD
     
-=======
 	private int tickNumber, sun, totalZombies;
-	
+    
+    /**
+     * 
+     */
 	private Game()
 	{
 		
 	}
-	
+    
+    /**
+     * 
+     */
 	private void splashScreen()
 	{
 		
 	}
-	
+    
+    /**
+     * 
+     */
 	public static void main(String[] args)
 	{
 		
 	}
->>>>>>> 52ac9e45188320db2c5c597e638d95e93878bbcb
 }
