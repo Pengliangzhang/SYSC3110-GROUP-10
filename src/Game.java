0f811bbaa594a6952ead760a0a59e6523b2153f9
unknown <xinruili3@cmail.carleton.ca>
2018-10-21 16:24:02 -0400

    Comment in game class

--- src/Game.java
+++ src/Game.java
@@ -1,20 +1,52 @@
+import java.util.ArrayList;
 
 public class Game
 {
-	private int tickNumber, sun, totalZombies;
-	
-	private Game()
+    private Place[][] map;
+    private int tickNumber, sun, totalZombies;
+    private ArrayList<Zombie> ZinR1;
+    private ArrayList<Plant> 
+
+    private Game() 
+    {
+        map = new Place[1][10];
+        sun = 0;
+        totalZombies = 10; // might be changed
+        tick = 0;
+        splashScreen();
+    }
+
+    private void splashScreen()
 	{
-		
+		// taketurn(); // begin the turn
 	}
-	
-	private void splashScreen()
+
+    private taketurn() 
+    {
+        tick += 10; // increase the sun
+        
+        // Prompt user
+
+        // Plant turn
+
+        // Check winner
+        if (totalZombies == 0) {
+            // Player win
+        }
+
+        // Zombie spawn
+
+        // Zombie turn
+
+        if (0) {
+            // Zombie win
+        } else {
+            taketurn();
+        }
+    }
+
+    public static void main(String[] args)
 	{
-		
-	}
-	
-	public static void main(String[] args)
-	{
-		
-	}
+
+    }
 }
