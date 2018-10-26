72b37f7adde76fbd543cf7926efff0ba85e13345
pengliangzhang <Pengliangzhang@cmail.carleton.ca>
2018-10-26 15:21:06 -0400

    set flower method in Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,99 +1,96 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Game
-{
-<<<<<<< HEAD
-    private ArrayList<Zombie> Zombie = new ArrayList();
-
-=======
->>>>>>> fc217f53adf9b31dc1f370d6f7fd8259714a7307
+public class Game {
 	private int tickNumber, sun, totalZombies;
-    
-    /**
-     * 
-     */
-	private Game()
-	{   
-        sun = 0;
-        totalZombies = 10; // might be changed
-        tick = 0;
+	private ArrayList<Sunflower> sunflower = new ArrayList<Sunflower>();
+	private ArrayList<Zombie> zombieList = new ArrayList<Zombie>();
+
+	/**
+	 * 
+	 */
+	private Game() {
+		sun = 0;
+		totalZombies = 10; // might be changed
+		tickNumber = 0;
 		titleScreen();
 	}
-	
-	private void titleScreen()
-	{
+
+	private void titleScreen() {
 		System.out.println("Welcome to SYSC3110 Group 10's PvZ, Console Vers.");
-		
+
 		Scanner console = new Scanner(System.in);
 		boolean goodInput = false;
 		String input = "";
-		while (!goodInput)
-		{
+		while (!goodInput) {
 			System.out.println("Enter \"play\" to play, and \"exit\" to quit.");
 			input = console.nextLine();
-			if (input.equals("play") || input.equals("exit"))
-			{
+			if (input.equals("play") || input.equals("exit")) {
 				goodInput = true;
 				console.close();
 			}
 		}
-		
-<<<<<<< HEAD
-    }
-    
-    /**
-     * @
-     */
-    public void changePosition(){
-        for (Zombie zombie : Zombie) {
-            zombie.
-        }
-    }
-=======
-		if (input.equals("play"))
-		{
+	}
+
+	/**
+	 * @
+	 */
+	public void changePosition(Zombie curr) {
+		for (Zombie zombie : Zombie) {
+			if (input.equals("play")) {
+				taketurn();
+			} else if (input.equals("exit")) {
+				System.exit(0);
+			}
+
+		}
+	}
+
+	@SuppressWarnings("unused")
+	private void taketurn() {
+		tickNumber += 10; // increase the sun
+
+		// Prompt user
+
+		// Plant turn
+
+		// Check winner
+		if (totalZombies == 0) {
+			// Player win
+		}
+
+		// Zombie spawn
+
+		// Zombie turn
+
+		if (false) {
+			// Zombie win
+		} else {
 			taketurn();
 		}
-		else if (input.equals("exit"))
-		{
-			System.exit(0);
-		}
-<<<<<<< HEAD
 	}
->>>>>>> fc217f53adf9b31dc1f370d6f7fd8259714a7307
-=======
-    }
-    
-    private taketurn() 
-    {
-        tick += 10; // increase the sun
-        
-        // Prompt user
+	
+	/** 
+	 * @desc this method will set the position for the sun flowers
+	 * @author BeckZ
+	 * @param x, the x position for the sun flower
+	 * @param y, the y position for the sun flower
+	 * @return null
+	 */
+	public void setflower(int x, int y) {
+		Sunflower flower = new Sunflower(x, y);
+		if(sunflower.add(flower)) {
+			System.out.println("You create an new sunflower.");
+		}else {
+			System.out.println("Unable to create a new sunflower!");
+		}
+		
+	}
 
-        // Plant turn
+	/**
+	 * 
+	 */
+	public static void main(String[] args) {
 
-        // Check winner
-        if (totalZombies == 0) {
-            // Player win
-        }
-
-        // Zombie spawn
-
-        // Zombie turn
-
-        if (0) {
-            // Zombie win
-        } else {
-            taketurn();
-        }
-    }
->>>>>>> 60faf6bfb38bac26acf7cc46ff503ac2beb91247
-    
-    /**
-     * 
-     */
-	public static void main(String[] args)
-	{
-
-    }
+	}
 }
